537334ffe959ffbc97fe4c4d17117001c2d706de
Sheansuke <suerojean@gmail.com>
2024-03-23 17:42:01 -0400

    Update HeroImage.tsx

--- frontend/src/app/(public)/_components/HeroImage.tsx
+++ frontend/src/app/(public)/_components/HeroImage.tsx
@@ -3,6 +3,6 @@ import { useBreakPoints } from "@/infrastructure/hooks/useBreakPoints";
 import Image from "next/image";
 
 export const HeroImage = () => {
-  const { isDesktop } = useBreakPoints();
-  return <>{isDesktop ? <Image  priority src="/svg/HeroImage.svg" width={500} height={500} alt="" /> : null}</>;
+  const { isMobile } = useBreakPoints();
+  return <>{isMobile ? null : <Image  priority src="/svg/HeroImage.svg" width={500} height={500} alt="" />}</>;
 };
