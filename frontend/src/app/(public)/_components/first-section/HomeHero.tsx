4eaf4e505c0dca506bc17b18edf269ca371398c7
Sheansuke <suerojean@gmail.com>
2024-04-28 18:41:27 -0400

    make hero SSR

--- frontend/src/app/(public)/_components/first-section/HomeHero.tsx
+++ frontend/src/app/(public)/_components/first-section/HomeHero.tsx
@@ -1,8 +1,5 @@
-"use client";
-
 import { HeroImage } from "@/app/(public)/_components/first-section/HeroImage";
-import { logger } from "@/core/server-actions/logger";
-import { Suspense } from "react";
+
 
 export const HomeHero = () => {
   return (
@@ -12,20 +9,12 @@ export const HomeHero = () => {
         <div className="flex-1 text-center md:text-left">
           <h1 className="text-green-950 md:text-[4rem] text-[3rem] font-bold text-opacity-90">Guarderia De Danne</h1>
           <h2 className="text-green-800 md:text-[3rem] text-[2rem] text-opacity-90 mt-2">¡Donde cada niño encuentra su mundo de juego y aprendizaje!</h2>
-          <button
-            className="btn btn-lg bg-green-950 font-bold text-green-100 hover:bg-green-100 hover:text-green-950 border-none rounded-2xl bg-opacity-90 mt-6"
-            // TODO: Add whatsapp contact action
-            onClick={() => logger("I want to register my child")}
-          >
-            Quiero Inscribir a mi hijo
-          </button>
+          <button className="btn btn-lg bg-green-950 font-bold text-green-100 hover:bg-green-100 hover:text-green-950 border-none rounded-2xl bg-opacity-90 mt-6">Quiero Inscribir a mi hijo</button>
         </div>
 
         {/* IMAGE */}
         <div className="hidden md:block items-center">
-          <Suspense fallback={<div className="skeleton bg-opacity-10 w-[382.01px] h-[412.01px]"></div>}>
-            <HeroImage />
-          </Suspense>
+          <HeroImage />
         </div>
       </div>
     </div>
