2e1e3e28f8fd54d014420ea80466d27cae00c04b
Sheansuke <suerojean@gmail.com>
2024-09-08 17:57:48 -0400

    Update page.tsx

--- frontend/src/app/(public)/consultar-reporte/page.tsx
+++ frontend/src/app/(public)/consultar-reporte/page.tsx
@@ -16,7 +16,9 @@ export default async function ConsultarReporte({
       <div className="container flex flex-col gap-8 mt-8 text-center">
         <h1 className="text-orange-950 md:text-[2rem] text-[1rem] font-bold text-opacity-90">Consulte su informacion y el reporte de su hijo ingresando el CODIGO que le fue asignado</h1>
 
+        <Suspense fallback={null}>
         <SearchForm />
+        </Suspense>
       </div>
 
       <div className="md:mt-24 mt-14">
